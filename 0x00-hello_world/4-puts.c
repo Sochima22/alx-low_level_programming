ea19e9737a23723744a123d277bf4cc64874c781
sochima22 <andriansochima@gmail.com>
2022-09-08 15:18:21 -0700

    hero

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main-Entry point*
+ * description:Programming is like building a multilingual puzzle*
+ * Return:Always 0 (success)
+ */
+int main(void)
+{char Mylin[] = "Programming is like building a multilingual puzzle";
+	puts(Mylin);
+	return (0); }
