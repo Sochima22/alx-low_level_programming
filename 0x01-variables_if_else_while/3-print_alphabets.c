9797c4d191ba21fc197b0afa134267cacb794147
sochima22 <andriansochima@gmail.com>
2022-09-09 16:35:49 -0700

    vret

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main-main entry*
+ * Description:Both lower and upper case*
+ * Return:Always 0 (success)
+ */
+int main(void)
+{int ch;
+for
+	(ch = 'a';
+	 ch <= 'z';
+	 ch++);
+{
+	putchar(ch);
+	putchar('\n'); }
+for
+	(ch = 'A';
+	 ch <= 'Z';
+	 ch++);
+putchar('\n');
+putchar('\n');
+return (0);
+}
