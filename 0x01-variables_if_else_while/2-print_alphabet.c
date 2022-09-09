61998ba629a18d1862fd1d5b7bd6e97bad2cf30c
sochima22 <andriansochima@gmail.com>
2022-09-09 15:14:57 -0700

    so

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-int ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-putchar(ch);
-putchar('\n');
-return (0);
+	int ch; for
+		(ch = 'a'; ch <= 'z'; ch++);
+	putchar(ch);
+	putchar('\n');
+	return (0);
 }
