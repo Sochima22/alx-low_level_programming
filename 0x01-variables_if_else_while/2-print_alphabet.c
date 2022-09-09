1057ea3aa79f4b11d6a84f8dc7db3f85a1949a45
sochima22 <andriansochima@gmail.com>
2022-09-09 09:34:13 -0700

    ok

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main-Entry function*
+ * Description:prints the alphabet in lowercase, followed by a new line.*
+ * Return:Always 0 (sucess)
+ */
+int main(void)
+{
+	int x; for (x = 'A'; x = 'Z'; x++);
+{
+	putchar(x); }
+	return (0); }
