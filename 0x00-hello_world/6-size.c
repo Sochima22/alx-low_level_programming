adc71d7efebff076eaac868c9b7a4f73f6941abd
sochima22 <andriansochima@gmail.com>
2022-09-08 18:39:59 -0700

    huh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  * Return:Always 0 (success)*
  */
 int main(void)
-{printf("Size of char: %d byte(s)\n", (int) sizeof(char));
+{printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
 printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
