75115d1bc06fdf2a9ef4a3bfac4048f881ebe2cf
sochima22 <andriansochima@gmail.com>
2022-09-09 14:45:42 -0700

    hh

---
+++ 0x01-variables_if_else_while/4-print_alphabets.c
@@ -0,0 +1,14 @@
+#include<stdio.h>                                 /**
+ * main-Main function*
+ *Description:Write a program that prints the alphabet in lowercase*
+ * Return:Always 0 (success)
+ */
+int main(void)
+{ int x;
+	for (x = 'a'; x <= 'z'; x++)
+	{if
+		(x == 'e' && x == 'q')
+		{continue; }
+		putchar(x);
+		putchar('\n'); }
+	return (0); }
