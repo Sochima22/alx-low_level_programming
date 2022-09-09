b49f26c83118d9b739704234fe1c59a2273fb6c1
sochima22 <andriansochima@gmail.com>
2022-09-08 19:48:17 -0700

    very

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ *main-Main file*
+ * description:prints exactly and that piece of art is useful*
+ * Return:turns (1)
+ */
+int main(void)
+{fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1); }
