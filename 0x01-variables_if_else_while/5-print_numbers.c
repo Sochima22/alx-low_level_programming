cbe445c4f869f31e5048e0b29c8dff63a84d6623
sochima22 <andriansochima@gmail.com>
2022-09-10 04:08:15 -0700

    so

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main-entry point*
+* Description: print 0 to 10 in new line*
+* Return:Always 0 (success)
+*/
+int main(void)
+{ char ch;
+	for (ch = 0; ch < 10; ch++)
+		putchar(ch);
+	return (0); }
