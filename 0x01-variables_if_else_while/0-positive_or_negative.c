af062429f8b3629e54afd67c4930c0e75e954c8a
sochima22 <andriansochima@gmail.com>
2022-09-10 12:42:55 -0700

    so

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if
 		(n > 0)
-		{printf("%d is negative", n); }
+		{printf("%d is positive", n); }
 	else
 		if
 			(n == 0)
-			{printf("is zero %d", n); }
+			{printf("%d is zero", n); }
 				else
-{printf("is negative %d", n); }
+{printf("%d is negative", n); }
 return (0); }
