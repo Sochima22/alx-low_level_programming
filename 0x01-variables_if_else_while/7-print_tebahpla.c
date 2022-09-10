e43a493793a160fc05a2a2f41a1f0de920f92443
sochima22 <andriansochima@gmail.com>
2022-09-10 04:26:28 -0700

    do

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* main- main entry*
+* Description: print alphabet in reverse*
+* Return: always 0
+*/
+int main(void)
+{ int x;
+	for (x = 'z'; x <= 'a'; x++)
+	{putchar (x); }
+	putchar('\n');
+	return (0); }
