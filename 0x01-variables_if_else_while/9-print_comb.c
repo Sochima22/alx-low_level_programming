6e357c13752c8f72b5617729e3f115ff727c0b3e
sochima22 <andriansochima@gmail.com>
2022-09-11 06:15:39 -0700

    kill

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+* main-entry point*
+* Description: print 0 to 10 in new line*
+* Return:Always 0 (success)
+*/
+int main(void)
+{ int ch;
+	for (ch = 0; ch <= 9; ch++)
+	{putchar((ch % 10) + '0');
+			if (ch == 0)
+			continue;
+			putchar(',');
+			putchar(' '); }
+	putchar('\n');
+	return (0); }
