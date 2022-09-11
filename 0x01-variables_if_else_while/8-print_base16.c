af0de1304614576f670576be6f53f2cc445ef140
sochima22 <andriansochima@gmail.com>
2022-09-10 18:01:59 -0700

    jj

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main-main entry*
+ * Description:Both lower and upper case*
+ * Return:Always 0 (success)
+ */
+int main(void)
+{int x = 0x0;
+
+	while
+		(x >= 0x0);
+	{putchar(x);
+		x++; }
+	putchar('\n');
+	return (0); }
