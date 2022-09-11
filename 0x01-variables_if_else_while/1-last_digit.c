886b37d993d2fdfbf82d0e5caa1381bdb4f78ba7
sochima22 <andriansochima@gmail.com>
2022-09-11 15:26:56 -0700

    go

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,9 +11,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	while
-		(n == 0)
+		(n != 0)
 		{printf("Last digit of %d is 0 and is 0\n", n); }
 	if
 		(n < 6 || n == 0)
-		{printf("Last digit of %d is - 8 and is less than 6 and not 0\n", n); }
+		{printf("Last digit of %d is -8 and is less than 6 and not 0\n", n); }
 	return (0); }
