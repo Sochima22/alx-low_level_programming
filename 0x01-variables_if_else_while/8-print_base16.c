903e47de1c988ab6d17f98bd5b80aa96fe987c16
sochima22 <andriansochima@gmail.com>
2022-09-10 19:01:38 -0700

    bo

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,9 +5,9 @@
  * Return:Always 0 (success)
  */
 int main(void)
-{int y = 0;
+{int y = 0x30;
 char x = 0x61;
-while (y >= 0 && y <= 9)
+while (y >= 0x30 && y <= 0x39)
 {putchar(y);
 	y++; }
 	while (x >= 0x61 && y <= 0x62)
