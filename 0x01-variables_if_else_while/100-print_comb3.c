8157b7c5ffec4e5cbbb4e72d017f89971832f340
sochima22 <andriansochima@gmail.com>
2022-09-11 12:27:23 -0700

    go

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,10 +7,11 @@
 int main(void)
 { int ch;
 	for (ch = 0; ch <= 99; ch++)
-	{putchar((ch % 100) + '0');
-			if (ch == 99)
+	{putchar((ch / 10) + '0');
+		putchar((ch % 10) + '0');
+		if (ch == 9)
 			continue;
-			putchar(',');
-			putchar(' '); }
+		putchar(',');
+		putchar(' '); }
 	putchar('\n');
 	return (0); }
