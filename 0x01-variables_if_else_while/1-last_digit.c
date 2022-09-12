4077ee16cb128431e66065cb8d007d60f62a9f1a
sochima22 <andriansochima@gmail.com>
2022-09-12 04:06:15 -0700

    job

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if
-		(n < 5 && n == 0)
+		(n < 5 || n == 0)
 		{printf("Last digit of %d is 8 and is greater than 5\n", n); }
 	else
 		if
