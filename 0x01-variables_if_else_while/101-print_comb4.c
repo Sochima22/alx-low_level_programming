c2587fc487ee386fd26462853f8e16d170cec60e
sochima22 <andriansochima@gmail.com>
2022-09-11 17:32:25 -0700

    kill

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,4 +4,20 @@
  * Description: i dont know*
  * Return:Always 0 (success)
  */
-
+int main(void)
+{int a, b, c;
+	for (a = 0; a < 8; a++)
+	{
+		for (b = a + 1; b < 9; b++)
+		{
+			for (c = b + 1; c < 10; c++)
+			{
+				if (a != b && a != c && b != c)
+				{putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					if (a + b + c < 24)
+					{putchar(',');
+						putchar(','); }}}}}
+	putchar('\n');
+	return (0); }
