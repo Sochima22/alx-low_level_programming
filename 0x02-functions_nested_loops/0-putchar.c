ceed0201fb804ffb5d2b5392e21048ebbe795f79
sochima22 <andriansochima@gmail.com>
2022-09-13 08:37:14 -0700

    huh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include"main.h"
 /**
 *main- Main entry*
@@ -5,10 +6,15 @@
 *Return: Always 0 (success)
 */
 int main(void)
-{char c[] = "_putchar";
-	int h;
-	for (h = 0; h <= 8; h++)
-	{_putchar(c[h]); }
-	return (0); }
+{{_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n'); }
+	return(0); }
 
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,1 @@
-int _putchar(char c[], int h);
+int _putchar(char c);
