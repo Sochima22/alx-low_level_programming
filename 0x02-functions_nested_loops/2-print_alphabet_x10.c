41034de9046a1123ec1fd2ef5a45392f19ae9e61
sochima22 <andriansochima@gmail.com>
2022-09-13 11:48:14 -0700

    huh

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*print_alphabet- Main entry*
+*Description: funtions prototypes*
+*Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{int c,
+	int i;
+
+	for (i = 0;
+			i <= 10
+			i++)
+		for (c = 'a'; c <= 'z'; c++)
+		{_putchar(c); }
+	_putchar('\n'); }}
