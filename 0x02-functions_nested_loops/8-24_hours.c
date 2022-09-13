20e319bdaef93c5c4dd8d904b4852bbdfe3d3562
sochima22 <andriansochima@gmail.com>
2022-09-13 16:19:39 -0700

    huh

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,14 +12,14 @@ void jack_bauer(void)
 	while (i < 24)
 	{j = 0;
 		while (j < 60)
-		{_putchar((i \ 10) + '0');
+		{_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(';');
-			_putchar((j \ 10) + '0');
+			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
 			j++;
 		}
-		i++
+		i++;
 	}}
 
