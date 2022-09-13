1b2dd54eeebd70da6fe37f4c4c61769ac303e40b
sochima22 <andriansochima@gmail.com>
2022-09-13 09:52:15 -0700

    huh

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*main- Main entry*
+*description: Functions prototypes*
+*Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{int c;
+for (c = 'a'; c >= 'z'; c++)
+{_putchar(c);}
+_putchar('\n'); }
+	int main(void)
+{print_alphabet();
+	return (0); }
+
+
