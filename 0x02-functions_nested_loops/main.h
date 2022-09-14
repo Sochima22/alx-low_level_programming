a86d66119809b0aaf683679c591acb83137ac4eb
sochima22 <andriansochima@gmail.com>
2022-09-14 12:47:24 -0700

    kill

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,5 @@ int print_last_digit(int);
 void jack_bauer(void);                       
 void times_table(void);                     
 int add(int, int);                           
-void print_to_98(int n);                    
-void time_table(void);
+void print_to_98(int n);                   
 void print_times_table(int n);
