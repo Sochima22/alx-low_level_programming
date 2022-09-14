d2c7ec1fc7ce084c29184f69aa50e0d4645cf517
sochima22 <andriansochima@gmail.com>
2022-09-14 12:40:54 -0700

    kill

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,17 +16,15 @@ for
 		 j <= 9;
 		 j++);
 	k = j * i;
-	if (j == '0')
+	if (j == 0)
 	{_putchar('0'); }
 	else if (k <= 9)
 	{_putchar(',');
-		_putchar('.');
 		_putchar(' ');
 		_putchar(k + '0'); }
 	else
 	{
 		_putchar(',');
-		_putchar('.');
 		_putchar(' ');
 		_putchar((k / 10) + '0');
 		_putchar((k % 10) + '0'); }}
