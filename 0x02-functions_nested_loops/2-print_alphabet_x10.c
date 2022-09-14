a0acebd9a0aada1e4048e5cf567a53484f6be9cc
sochima22 <andriansochima@gmail.com>
2022-09-13 17:03:38 -0700

    huh

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,15 @@
 */
 void print_alphabet_x10(void)
 {int ch;
-	int i;
-	for(i = 0; i <= 9; i++);
+	int i = 48;
+	while (i <= 57)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
 		_putchar('\n');
+		i++
 		}
 	return; }
 
