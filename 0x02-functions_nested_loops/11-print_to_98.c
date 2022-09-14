e08e7c917155bbe7ba107ad54284ab87e8fcff8e
sochima22 <andriansochima@gmail.com>
2022-09-14 11:34:04 -0700

    lo

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*print_to_98-print all matural nu,bers fron n to 98*
+*@n:print from this number
+*/
+void print_to_98(int n)
+{int i,
+	j;
+
+	if  (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			_putchar(i);
+			else if
+				(i == 98)
+					_putchar(i); }}
+	else if
+		(n >= 98)
+		{
+			{
+				if (j != 98)
+				_putchar(j);
+else if
+	(j == 98) _putchar(j); }}}
