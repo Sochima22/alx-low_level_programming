0a3569d59fe0db1352f6cc8434da2ce6815dabad
sochima22 <andriansochima@gmail.com>
2022-09-13 18:23:22 -0700

    kill

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include"main.h"
+/**
+*time_table-prints the ninth times table*
+*description:i dont know*
+*/
+void time_table(void)
+{int i, j. k;
+
+for
+	(i = 0;
+	 i < 10;
+	 i++);
+{
+	for
+		(j = 0;
+		 j < 0;
+		 j++);
+	{
+		k = j * 1;
+		if (j == 0)
+		{_putchar(k + '0'); }
+		if (k < 10 && j != 0)
+{_putchar(',');
+_putchat(' ');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}}
+_putchar('\n'); }}
