26c1b545770bdfedfcafa078b2e1acf9574ef558
sochima22 <andriansochima@gmail.com>
2022-09-13 20:06:38 -0700

    ji

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+*description:add two numbers and return the result*
+*@a:int to added to b*
+*@b:int to addec to a*
+Return:c(sum of a and b)
+*/
+int add(int a, int b)
+{int c;
+
+	c = a + b;
+
+	return (c); }
