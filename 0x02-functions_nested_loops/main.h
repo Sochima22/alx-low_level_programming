c2fbc7f22432b72f6d8e316c68d456fcec5326c9
sochima22 <andriansochima@gmail.com>
2022-09-14 10:17:04 -0700

    huh

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,4 +12,5 @@ void jack_bauer(void);
 void times_table(void);                     
 int add(int, int);                           
 void print_to_98(int n);                    
+void time_table(void);
 void print_times_table(int n);
