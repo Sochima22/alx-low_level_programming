024e913864dcd2dd015d6d05893f1088509c3a5b
sochima22 <andriansochima@gmail.com>
2022-09-13 18:29:34 -0700

    kill

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
 *description:i dont know*
 */
 void time_table(void)
-{int i, j. k;
+{int i, j, k;
 
 for
 	(i = 0;
@@ -21,7 +21,7 @@ for
 		{_putchar(k + '0'); }
 		if (k < 10 && j != 0)
 {_putchar(',');
-_putchat(' ');
+_putchar(' ');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
