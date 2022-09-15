f1f658b088db72473b48763d3b2f7f7de5f335c0
sochima22 <andriansochima@gmail.com>
2022-09-14 21:47:16 -0700

    kil

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,17 +12,19 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			k = j * i;
-			if (j == 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(k + '0'); }
-			else if (k < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0'); }
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0'); }
+				else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0'); }
 				else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
@@ -32,6 +34,7 @@ void print_times_table(int n)
 					_putchar((k % 10) + '0'); }
 				else if (k >= 100)
 				{
+
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
@@ -39,4 +42,4 @@ void print_times_table(int n)
 					_putchar((k % 10) + '0');
 				}}
 			_putchar('\n');
-		}}
+		}}}
