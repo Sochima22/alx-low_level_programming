ec16c23bba844b114dcd466c1400fa73d51809de
sochima22 <andriansochima@gmail.com>
2022-09-16 09:35:49 -0700

    kil

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,8 +5,12 @@
 */
 void print_most_numbers(void)
 {int c;
-	for (c = 0; c <= 9; c++)
-	{_putchar((c % 10) + '0');
-		if (c == 2 || c == 4)
-		continue; }
-		_putchar('\n'); }
+	for (c = 48; c <= 58; c++)
+	{
+		if (c == 50 || c == 53)
+		{
+			continue;
+		}
+		_putchar(c); 
+	}
+	_putchar(10);
