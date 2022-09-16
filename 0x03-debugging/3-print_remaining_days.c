f32e9b524fb0cba1dad7ff241c0fbd6735cd8848
sochima22 <andriansochima@gmail.com>
2022-09-15 17:57:36 -0700

    ki

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,8 +14,8 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month > 2)
 			day++;
-		printf("Day of the year: %d", day);
-		printf("Remaining days: %d", 366 - day); }
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day); }
 	else
 	{
 		if (month == 2 && day == 60)
