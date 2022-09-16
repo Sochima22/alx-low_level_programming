aace0815ce5b990cd8a42f2f2d0cf7d19befb4e4
sochima22 <andriansochima@gmail.com>
2022-09-16 08:52:55 -0700

    huh

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+*_isupper-checks if a chararcter is uppercase or not*
+*@c:character to be tested*
+*Return:1 if it is, 0 if otherwise
+*/
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,4 +5,12 @@ int _putchar(char);
 void print_number(int n);
 void print_most_numbers(void);
 void print_numbers(void);
+void more_numbers(void);
+void print_diagonal(int n);
+void print_line(int n);
+void print_square(int size);
+void print_triangle(int size);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 #endif
