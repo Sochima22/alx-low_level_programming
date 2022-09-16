fcf7a7c34626eb8d5ce1e6db48c695c8298a6ee2
sochima22 <andriansochima@gmail.com>
2022-09-16 10:23:46 -0700

    kill

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+*more_numbers-prints numbers to 14*
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; i++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n'); }}
