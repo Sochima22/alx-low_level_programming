3141ba05d55a2ec423b3c2da5b56e3312d3c369b
sochima22 <andriansochima@gmail.com>
2022-09-16 19:09:47 -0700

    kill

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,14 @@ int main(void)
 	while (num++ < 100)
 	{
 		if ((num % 3 == 0) && (num % 5 == 0))
-		{
 			printf("FizzBuzz");
-		}
 		else if ((num % 3) == 0)
-		{
 			printf("Fizz");
-		}
 		else if ((num % 5) == 0)
-		{
 			printf("Buzz");
-		}
 		else
-		{
 			printf("%d", num);
-		}
-		printf("Buzz\n");
-		return (0);
+	}
+	printf("Buzz\n");
+	return (0);
 }
