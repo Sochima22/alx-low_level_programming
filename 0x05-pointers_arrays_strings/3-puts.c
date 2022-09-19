f855f0ccc53860e57d5c27bca58a28348d060155
sochima22 <andriansochima@gmail.com>
2022-09-19 14:04:10 -0700

    lol

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*_puts-primts a string followed by new line
+*@str:pointer to the string
+*Return:void
+*/
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchat('\n');
+}
