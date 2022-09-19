1d8a807c4a98ff1986ec54473f6660b1c515caa7
sochima22 <andriansochima@gmail.com>
2022-09-19 16:07:22 -0700

    lo

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,13 @@
-#incluse"main.h"
+#include"main.h"
 /**
-*puts2- prints every other character of a string from first character
+*put2-prints every other character of a string from first character
 *@str: pointerto the string
 *Return:void
 */
 void put2(char *str)
 {
 	int i = 0;
+
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
