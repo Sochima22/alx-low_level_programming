7617af3ee92a0b7089eda79bc04c0edf24321cc0
sochima22 <andriansochima@gmail.com>
2022-09-19 16:54:15 -0700

    kill

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+*puts_half-prints second half of a string
+*@str:pointer to char array
+*/
+void puts_half(char *str)
+{
+	int i;
+
+	for
+		(i = 0;
+		 str[i] != '\0';
+		 i++);
+	i++
+		;
+	for
+		(i /= 2;
+		 str[i] != '\0'
+		 i++);
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n);
+}
