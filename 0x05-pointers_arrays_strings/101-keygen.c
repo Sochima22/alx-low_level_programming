54992f47c93f580fded5238943b69b4dd0002d95
sochima22 <andriansochima@gmail.com>
2022-09-19 18:28:26 -0700

    lo

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -32,10 +32,18 @@ int main(void)
 		for (index = 0; password[index]; index++)
 		{
 			if
+				(password[index] >= (33 + diff_half1))
+				{
+					password[index] -= diff_half1;
+					break;
+				}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if
 				(password[index] >= (33 + diff_half2))
 				{
 					password[index] -= diff_half2;
-					break;
 				}
 		}
 	}
