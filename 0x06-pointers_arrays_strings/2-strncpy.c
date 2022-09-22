a102e797d7ae2b47af3c6fd5303ef80738a2fd6b
sochima22 <andriansochima@gmail.com>
2022-09-22 14:17:05 -0700

    lol

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include"main.h"
+/**
+**_strncpy-copy strings
+*@dest:destination pointer
+*@src:source pointer
+*@n:number of characters to copy
+*Return:returns characters
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x = 0;
+
+	while (x < n && src[x] != '\0')
+	{
+		dest[x] = src[x];
+		x++;
+	}
+
+	while (x < n)
+	{
+		dest[x] != '\0';
+		x++;
+	}
+	return (dest);
+}
