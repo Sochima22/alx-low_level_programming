1187f3e533a0d6e13dbf246b113126214708ac23
sochima22 <andriansochima@gmail.com>
2022-09-22 05:09:26 -0700

    lol

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+/**
+**_strcat-concatenates two strings
+*@dest:destination pointer
+*@src:source pointer
+*Return:returns characters
+*/
+char *_strcat(char *dest, char *src)
+{
+	len1 = strlen(*dest);
+	len2 = strlen(*sec);
+	for (*src = 0; src <= len1; src++)
+	{
+		len2 = (len1 + *src);
+	}
+	_putchar(len2); }
+
