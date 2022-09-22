33a5c8a83901bb2e8ab9d36369365880ec1ad46a
sochima22 <andriansochima@gmail.com>
2022-09-22 13:50:10 -0700

    lol

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include"main.h"
+/**
+**_strncat-copy strings
+*@dest:destination pointer
+*@src:source pointer
+*@n:number of characters to copy
+*Return:returns characters
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int x = 0;
+	int y = 0;
+
+	while (*(dest + x) != '\0')
+		x++;
+
+	while (*(src + y) != '\0' && y < n)
+	{
+		*(dest + x) = *(src + x);
+		x++;
+		y++;
+	}
+	*(dest + x) = '\0';
+	return (dest);
+}
