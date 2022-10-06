da4bd8c8407bb3bee6731e30cc40feecb40678a0
sochima22 <andriansochima@gmail.com>
2022-10-06 09:16:12 -0700

    #$

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,56 +1,33 @@
-#include"main.h"
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include "main.h"
 
 /**
-*simple_print_buffer - prints buffer in hexa
-*
-*@buffer: the address of memory to print
-*@size: the size of the memory to print
-*
-*Return: Nothing.
+*_realloc -  reallocates a memory block using malloc and free
+*@ptr: pointer
+*@old_size: old size
+*@new_size: new size
+*Return: pointer
 */
-void simple_print_buffer(char *buffer, unsigned int size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	char *clone, *relloc;
 	unsigned int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
-}
-
-/**
-*main - check the code for ALX School students.
-*
-*Return: Always 0.
-*/
-
-int main(void)
-{
-	char *p;
-	int i;
-
-	p = malloc(sizeof(char) * 10);
-	p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
-	i = 0;
-	while (i < 98)
-	{
-		p[i++] = 98;
-	}
-	simple_print_buffer(p, 98);
-	free(p);
+	if (ptr != NULL)
+	clone = ptr;
+	else
+	{ return (malloc(new_size)); }
+	if (new_size == old_size)
+	return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{ free(ptr);
+	return (0); }
+	relloc = malloc(new_size);
+	if (relloc == NULL)
 	return (0);
+	for (i = 0; i < (old_size || i < new_size); i++)
+	{
+		*(relloc + i) = clone[i];
+	}
+	free(ptr);
+return (relloc);
 }
