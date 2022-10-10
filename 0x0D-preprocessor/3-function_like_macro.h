6f601f2e88a5339b14bcae6d59e90d601af93544
sochima22 <andriansochima@gmail.com>
2022-10-10 05:16:44 -0700

    killing it

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,19 @@
-#ifndef _FUNCTION_LIKE_MACRO_H
-#define _FUNCTION_LIKE_MACRO_H
+#include<stdio.h>
+#include"3-function_like_macro.h"
+#include"3-function_like_macro.h"
+/**
+*main - check the code for ALX School students.
+*
+*Return: Always 0.
+ */
 
-#define ABS(x) (((x) >= 0) ? (x) : -(x))
+int main(void)
+{
+	int i;
+	int j;
 
-#endif /* _FUNCTION_LIKE_MACRO_H
+	i = ABS(-98) * 10;
+	j = ABS(98) * 10;
+	printf("%d, %d\n", i, j);
+	return (0);
+}
