0953618fb415d86b84cb35282bd8369b5e16c0ca
sochima22 <andriansochima@gmail.com>
2022-10-25 13:30:00 -0700

    not yet uhuru

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,66 @@
 #include"lists.h"
 /**
-* print_listint_safe - prints a linked list, safely
-* @head: list of type listint_t to print
+* free_listp - frees a linked list
+* @head: head of a list.
+* Return: no return.
+*/
+void free_listp(listp_t **head)
+{
+	listp_t *t;
+	listp_t *c;
+
+	if (head != NULL)
+	{
+		c = *head;
+		while ((t = c) != NULL)
+		{
+			c = c->next;
+			free(t);
+		}
+		*head = NULL;
+	}
+}
+/**
+* print_listint_safe - prints a linked list.
+* @head: head of a list.
 *
-* Return: number of nodes in the list
+* Return: number of nodes in the list.
 */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t num = 0;
-	long int diff;
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
 
-	while (head)
+	hptr = NULL;
+	while (head != NULL)
 	{
-		diff = head - head->next;
-		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (nnodes);
+			}
 		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nnodes++;
 	}
 
-	return (num);
+	free_listp(&hptr);
+	return (nnodes);
 }
