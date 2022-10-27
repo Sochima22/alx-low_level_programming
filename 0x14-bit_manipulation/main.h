6c09fb3a27b8addc50d0e9c9d72e5bc388f74b31
sochima22 <andriansochima@gmail.com>
2022-10-27 11:39:48 -0700

    in the horah

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,7 +6,9 @@
 
 #ifdef DEBUG /*prints debug statements*/
 #endif
-
+int _length(unsigned long int n);
+void debug_3(unsigned long int mask);
+void debug_2(unsigned long int n, unsigned long int mask);
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -15,5 +17,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-
+void debug_1(unsigned long int n, int length, unsigned long int mask);
 #endif
